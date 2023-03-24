e9d3ba1ec2f21d359a4e8eab5f6a35b5bd9136cb
abado94 <abado94@yahoo.com>
2023-03-24 14:32:22 +0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/2bdd8359-1dcc-4c28-9dbc-b3981bd95d74/guestbook/';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
